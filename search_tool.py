ed81ab0cdf9f840cf9094dc4b10f7e4e3fe9b5bb
Joseph Cho <ujcho@jacksongov.org>
2025-11-05 16:11:41 -0600

    update color presentation in search tool

--- search_tool.py
+++ search_tool.py
@@ -12,7 +12,8 @@ report_number = st.text_input(label="Enter your Police Report Number", label_vis
 
 if st.button("Search", icon=":material/search:"):
     if len(report_number) >= 5: 
-        result = df[df['Police Report Number'].str.contains(report_number, case=False, na=False)]
+        # result = df[df['Police Report Number'].str.contains(report_number, case=False, na=False)]
+        result = df[df['Police Report Number'].fillna('').str.lower() == report_number.lower()] # Exact string matches, instead of all partial matches; many NA (missing) report_nums in table
         if not result.empty:
             total_cases = len(result)
             counter = 0
@@ -23,15 +24,15 @@ if st.button("Search", icon=":material/search:"):
                 counter += 1
                 
                 if row['Case Status'].upper()=='RECEIVED':
-                    st.write(f"Case ({counter}/{total_cases}) was :red[**RECEIVED**] on {row['Date Received']} and submitted by the {row['Submitting Agency']}.\nThis case is likely under review and pending criminal charges.")
+                    st.write(f"Case ({counter}/{total_cases}) was :blue[**RECEIVED**] on :yellow-background[***{row['Date Received']}***] and submitted by the {row['Submitting Agency']}.\nThis case is likely under review and pending criminal charges.")
                 elif row['Case Status'].upper()=='FILED':
                     url = f"https://www.courts.mo.gov/cnet/cases/newHeader.do?inputVO.caseNumber={row['Court Number']}&inputVO.courtId=CT16&inputVO.isTicket=false"
-                    st.write(f"Case ({counter}/{total_cases}) was :red[**FILED**] by our Office on {row['Date Filed']} and was submitted by the {row['Submitting Agency']} for review on {row['Date Received']}.\nYou can find more information regarding this case on Case.net: [Court Number {row['Court Number']}]({url}).")
+                    st.write(f"Case ({counter}/{total_cases}) was :blue[**FILED**] by our Office on :yellow-background[***{row['Date Filed']}***] and was submitted by the {row['Submitting Agency']} for review on {row['Date Received']}.\nYou can find more information regarding this case on Case.net: [Court Number {row['Court Number']}]({url}).")
                 elif row['Case Status'].upper()=='NOT FILED':
-                    st.write(f"The Jackson County Prosecuting Attorney's Office :red[**DECLINED TO FILE CHARGES**] on Case ({counter}/{total_cases}) on {row['Date Not Filed']} due to the following reason(s): {row['Reason Not Filed']}.\nThis case was submitted by the {row['Submitting Agency']} for review on {row['Date Received']}.")
+                    st.write(f"The Jackson County Prosecuting Attorney's Office :blue[**DECLINED TO FILE CHARGES**] on Case ({counter}/{total_cases}) on :yellow-background[***{row['Date Not Filed']}***] due to the following reason(s): {row['Reason Not Filed']}.\nThis case was submitted by the {row['Submitting Agency']} for review on {row['Date Received']}.")
                 elif row['Case Status'].upper()=='DISPOSED':
                     url = f"https://www.courts.mo.gov/cnet/cases/newHeader.do?inputVO.caseNumber={row['Court Number']}&inputVO.courtId=CT16&inputVO.isTicket=false"
-                    st.write(f"Case ({counter}/{total_cases}) was :red[**DISPOSED**] on {row['Date Disposed']} with the following outcome(s): {row['Disposed Outcome']}.\nThis case was submitted by the {row['Submitting Agency']} for review on {row['Date Received']}.\nYou can find more information regarding this case on Case.net: [Court Number {row['Court Number']}]({url}).")
+                    st.write(f"Case ({counter}/{total_cases}) was :blue[**DISPOSED**] on :yellow-background[***{row['Date Disposed']}***] with the following outcome(s): {row['Disposed Outcome']}.\nThis case was submitted by the {row['Submitting Agency']} for review on {row['Date Received']}.\nYou can find more information regarding this case on Case.net: [Court Number {row['Court Number']}]({url}).")
             cleaned = result.dropna(axis=1, how='all')
             st.dataframe(cleaned, hide_index=True)
         else:
